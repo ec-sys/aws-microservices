b09a0a08c61886731fb6f3a1fea24970e2f1ae0a
long.dang <longthientai90@gmail.com>
2024-07-08 21:05:54 +0700

    add

---
+++ aws-service-product/src/main/java/demo/aws/backend/product/config/db/DbType.java
@@ -0,0 +1,6 @@
+package demo.aws.backend.product.config.db;
+
+public enum DbType {
+    MASTER,
+    SLAVE
+}
---
+++ aws-service-product/src/main/java/demo/aws/backend/product/config/db/RoutingDataSource.java
@@ -0,0 +1,13 @@
+package demo.aws.backend.product.config.db;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+public class RoutingDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        DbType dbType = TransactionSynchronizationManager.isCurrentTransactionReadOnly() ? DbType.SLAVE : DbType.MASTER;
+        return dbType;
+    }
+}
