f8556f77145e97631d3fe65026e4e820390a6498
long.dang <84171134+longdangdjv@users.noreply.github.com>
2024-07-14 15:07:16 +0700

    add

---
+++ aws-eureka-server/src/test/java/demo/aws/backend/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.aws.backend.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
